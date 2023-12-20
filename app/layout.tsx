883fdb2189c64704e2b01ce47e3ac7d6de78c60b
Vinicius H. <vnicidigital@gmail.com>
2023-12-20 19:30:05 -0300

    Ajustando deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { ReactNode } from 'react'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -9,13 +10,11 @@ export const metadata: Metadata = {
   description: 'Desenvolvedor Full-Stack',
 }
 
-type LayoutProps = { 
-  children: React.ReactNode 
-}
-
 export default function RootLayout({
   children,
-}: LayoutProps) {
+}: {
+  children: ReactNode
+}) {
   return (
     <html lang="pt-br" className='scroll-smooth'>
       <body className={inter.className}>{children}</body>
